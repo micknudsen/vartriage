3be0c4ed46cb70c0afacd0dddca3840a3e38b2a2
Michael Knudsen <micknudsen@gmail.com>
2020-10-19 20:55:36 +0200

    Parse variants for evidence VCFs

--- src/vartriage/client.py
+++ src/vartriage/client.py
@@ -1,4 +1,10 @@
+import gzip
+
 from argparse import ArgumentParser
+
+from vartriage.core import Variant
+from vartriage.parser import parse_vcf_entry
+from vartriage.triager import Triager
 
 
 def main():
@@ -9,3 +15,12 @@ def main():
     parser.add_argument('--evidence_vcfs', required=True)
 
     args = parser.parse_args()
+
+    triager = Triager()
+
+    for evidence_vcf in args.evidence_vcfs.split(','):
+        with gzip.open(evidence_vcf, 'rt') as f:
+            for line in f:
+                if not line.startswith('#'):
+                    variant = parse_vcf_entry(entry=line.rstrip('\n'))
+                    triager.add_evidence(variant=variant)
