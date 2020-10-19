4baae7b3268d3a2ef5db35c648edc1ff28b1b5bb
Michael Knudsen <micknudsen@gmail.com>
2020-10-19 20:29:32 +0200

    Implemented parse_vcf_entry function

--- src/vartriage/parser.py
+++ src/vartriage/parser.py
@@ -2,4 +2,13 @@ from vartriage.core import Variant
 
 
 def parse_vcf_entry(entry: str) -> Variant:
-    pass
+    chromosome, position, id, ref, alt, qual, filter, info, *genotypes = entry.split('\t')
+    return Variant(chromosome=chromosome,
+                   position=int(position),
+                   id=id,
+                   ref=ref,
+                   alt=alt,
+                   qual=qual,
+                   filter=filter,
+                   info=info,
+                   genotypes=genotypes)
