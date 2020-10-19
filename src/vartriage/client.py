9e96f904af6abaabf35c6fd7d5d38c9b993e2a04
Michael Knudsen <micknudsen@gmail.com>
2020-10-19 20:46:45 +0200

    Added an ArgumentParser

--- src/vartriage/client.py
+++ src/vartriage/client.py
@@ -1,2 +1,11 @@
+from argparse import ArgumentParser
+
+
 def main():
-    print('Hello, World!')
+
+    parser = ArgumentParser()
+
+    parser.add_argument('--triage_vcf', required=True)
+    parser.add_argument('--evidence_vcfs', required=True)
+
+    args = parser.parse_args()
