fb39c718ad5124db7544656a5f2c6cb2ceb93584
Michael Knudsen <micknudsen@gmail.com>
2020-10-19 20:02:00 +0200

    Added instance variables in Variant class

--- src/vartriage/core.py
+++ src/vartriage/core.py
@@ -1,2 +1,13 @@
-class Variant:
-    pass
+from typing import List, NamedTuple, Optional
+
+
+class Variant(NamedTuple):
+    chromosome: str
+    position: int
+    ref: str
+    alt: str
+    filter: str = '.'
+    id: str = '.'
+    qual: str = '.'
+    info: str = '.'
+    genotypes: Optional[List[str]] = None
