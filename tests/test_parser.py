ff0e3f3b29b8f0054e440d87f28b05a72cffaa34
Michael Knudsen <micknudsen@gmail.com>
2020-10-19 20:30:20 +0200

    Fixed broken test

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -19,5 +19,4 @@ class TestParser(unittest.TestCase):
         self.assertEqual(self.variant.qual, '.')
         self.assertEqual(self.variant.filter, 'PASS')
         self.assertEqual(self.variant.info, 'AS_FilterStatus=SITE;AS_SB_TABLE=18,152|0,4;DP=176;ECNT=3;GERMQ=93;MBQ=32,36;MFRL=286,178;MMQ=60,60;MPOS=3;NALOD=1.58;NLOD=10.82;POPAF=6.00;ROQ=93;TLOD=15.39')
-        self.assertEqual(self.variant.format, 'GT:AD:AF:DP:F1R2:F2R1:PGT:PID:PS:SB')
-        self.assertEqual(self.variant.samples, ['0|0:39,0:0.026:39:22,0:16,0:0|1:178575029_CAGAA_C:178575029:7,32,0,0', '0|1:131,4:0.039:135:57,3:65,1:0|1:178575029_CAGAA_C:178575029:11,120,0,4'])
+        self.assertEqual(self.variant.genotypes, ['GT:AD:AF:DP:F1R2:F2R1:PGT:PID:PS:SB', '0|0:39,0:0.026:39:22,0:16,0:0|1:178575029_CAGAA_C:178575029:7,32,0,0', '0|1:131,4:0.039:135:57,3:65,1:0|1:178575029_CAGAA_C:178575029:11,120,0,4'])
