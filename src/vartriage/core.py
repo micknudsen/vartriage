9bd0f65a8ccebb1901c7af0431bb7ab78d744eb3
Michael Knudsen <micknudsen@gmail.com>
2021-06-28 08:41:34 +0200

    Cleaned up

--- src/vartriage/core.py
+++ src/vartriage/core.py
@@ -42,15 +42,6 @@ class VCF():
                         samples=dict(zip(sample_names, samples)))
             )
 
-#     chromosome: str
-#     position: int
-#     ref: str
-#     alt: str
-#     filter: str = '.'
-#     id: str = '.'
-#     qual: str = '.'
-#     info: str = '.'
-#     genotypes: Optional[List[str]] = None
 
 #     def __post_init__(self):
 #         if len(self.alt.split(',')) > 1:
