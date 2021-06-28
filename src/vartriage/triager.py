081ada16b6f1c7c17dc8b8213416f316e1956e86
Michael Knudsen <micknudsen@gmail.com>
2021-06-28 10:12:35 +0200

    Now ready to start reimplementing the Triager class

--- src/vartriage/triager.py
+++ src/vartriage/triager.py
@@ -1,16 +1,16 @@
-# from typing import List
+from typing import List
 
-# from vartriage.core import Variant
+from vartriage.core import Variant
 
 
-# class Triager:
+class Triager:
 
-#     def __init__(self) -> None:
-#         self._evidence: List[Variant] = []
+    def __init__(self) -> None:
+        self._evidence: List[Variant] = []
 
-#     def add_evidence(self, variant: Variant) -> None:
-#         if not variant.is_filtered():
-#             self._evidence.append(variant)
+    def add_evidence(self, variant: Variant) -> None:
+        if not variant.is_filtered():
+            self._evidence.append(variant)
 
 #     def triage(self, variant) -> bool:
 #         return not variant.is_filtered() or variant in self._evidence
--- tests/test_triager.py
+++ tests/test_triager.py
@@ -1,23 +1,23 @@
-# import unittest
+import unittest
 
-# from vartriage.parser import parse_vcf_entry
-# from vartriage.triager import Triager
+from vartriage.core import Variant
+
+from vartriage.triager import Triager
 
 
-# class TestTriager(unittest.TestCase):
+class TestTriager(unittest.TestCase):
 
-#     def setUp(self):
+    def setUp(self):
 
-#         evidence_vcf_entries = ['\t'.join(['chr1', '9611912', '.', 'A', 'T', '.', 'LowEVS', 'SOMATIC;QSS=2;TQSS=1;NT=het;QSS_NT=2;TQSS_NT=1;SGT=AA->AA;DP=341;MQ=59.86;MQ0=0;ReadPosRankSum=-5.89;SNVSB=28.56;SomaticEVS=0.00', 'DP:FDP:SDP:SUBDP:AU:CU:GU:TU', '68:9:0:0:41,43:3,3:0,0:15,22', '273:28:0:0:190,193:4,6:0,2:51,72']),
-#                                 '\t'.join(['chr9', '129439117', '.', 'T', 'C', '.', 'PASS', 'SOMATIC;QSS=34;TQSS=2;NT=ref;QSS_NT=34;TQSS_NT=2;SGT=TT->CT;DP=101;MQ=60.00;MQ0=0;ReadPosRankSum=-0.11;SNVSB=0.00;SomaticEVS=8.04', 'DP:FDP:SDP:SUBDP:AU:CU:GU:TU', '21:0:0:0:0,0:1,1:0,0:20,20', '80:12:0:0:0,0:19,30:0,0:49,50']),
-#                                 '\t'.join(['chr15', '93301718', '.', 'G', 'T', '.', 'PASS', 'SOMATIC;QSS=35;TQSS=1;NT=ref;QSS_NT=35;TQSS_NT=1;SGT=GG->GT;DP=37;MQ=60.00;MQ0=0;ReadPosRankSum=0.78;SNVSB=0.00;SomaticEVS=9.32', 'DP:FDP:SDP:SUBDP:AU:CU:GU:TU', '9:0:0:0:0,0:0,0:9,9:0,0', '27:0:0:0:0,0:0,0:22,23:5,5']),
-#                                 '\t'.join(['chr20', '30388785', '.', 'T', 'G', '.', 'LowEVS', 'SOMATIC;QSS=5;TQSS=2;NT=ref;QSS_NT=5;TQSS_NT=2;SGT=TT->GT;DP=91;MQ=40.91;MQ0=6;ReadPosRankSum=-0.02;SNVSB=0.00;SomaticEVS=0.09', 'DP:FDP:SDP:SUBDP:AU:CU:GU:TU', '9:0:0:0:0,0:0,0:0,0:9,18', '53:2:0:0:0,0:0,0:3,4:48,69'])]
+        evidence_variants = [Variant(chrom='chr1', pos='9611912', id_='.', ref='A', alt='T', qual='.', filter_='LowEVS', info='SOMATIC;QSS=2', format_='DP:FDP:SDP', samples={'NORMAL': '68:9:0', 'TUMOR': '273:28:0'}),
+                             Variant(chrom='chr9', pos='129439117', id_='.', ref='T', alt='C', qual='.', filter='PASS', info='SOMATIC;QSS=34', format_='DP:FDP:SDP', samples={'NORMAL': '21:0:0:0:0,0:1,1:0,0:20,20', 'TUMOR': '80:12:0:0:0,0:19,30:0,0:49,50'}),
+                             Variant(chrom='chr15', pos='93301718', id_='.', ref='G', alt='T', qual='.', filter='PASS', info='SOMATIC;QSS=35', format_='DP:FDP:SDP', samples={'NORMAL': '9:0:0:0:0,0:0,0:9,9:0,0', 'TUMOR': '27:0:0:0:0,0:0,0:22,23:5,5'}),
+                             Variant(chrom='chr20', pos='30388785', id_='.', ref='T', alt='G', qual='.', filter='LowEVS', info='SOMATIC;QSS=5', format='DP:FDP:SDP', samples={'NORMAL': '9:0:0:0:0,0:0,0:0,0:9,18', 'TUMOR': '53:2:0:0:0,0:0,0:3,4:48,69'})]
 
-#         self.triager = Triager()
+        self.triager = Triager()
 
-#         for vcf_entry in evidence_vcf_entries:
-#             variant = parse_vcf_entry(entry=vcf_entry)
-#             self.triager.add_evidence(variant=variant)
+        for variant in evidence_variants:
+            self.triager.add_evidence(variant=variant)
 
 #     def test_triage_pass_variant_which_passes_in_evidence(self):
 #         variant = parse_vcf_entry(entry='\t'.join(['chr15', '93301718', '.', 'G', 'T', '.', 'PASS', 'AS_FilterStatus=SITE;AS_SB_TABLE=0,31|0,5;DP=37;ECNT=1;GERMQ=42;MBQ=36,36;MFRL=289,327;MMQ=60,60;MPOS=22;NALOD=1.000;NLOD=2.71;POPAF=6.00;ROQ=81;TLOD=13.99', 'GT:AD:AF:DP:F1R2:F2R1:SB', '0/0:9,0:0.091:9:4,0:5,0:0,9,0,0', '0/1:22,5:0.207:27:8,3:13,2:0,22,0,5']))
