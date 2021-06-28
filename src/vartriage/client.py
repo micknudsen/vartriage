3accce48c03e157060b90f570d20bddb3934f440
Michael Knudsen <micknudsen@gmail.com>
2021-06-28 15:31:59 +0200

    WIP

--- src/vartriage/client.py
+++ src/vartriage/client.py
@@ -1,33 +1,33 @@
-# import gzip
+import gzip
 
-# from typing import Dict, List
+from typing import Dict, List
 
-# from argparse import ArgumentParser
-# from vartriage.core import Variant
+from argparse import ArgumentParser
+from vartriage.core import Variant
 
-# from vartriage.parser import parse_vcf
-# from vartriage.triager import Triager
+from vartriage.parser import parse_vcf
+from vartriage.triager import Triager
 
 
 def main():
 
-    pass
+    parser = ArgumentParser()
 
-    # parser = ArgumentParser()
+    parser.add_argument('--triage_vcf_file', required=True, help='VCF file to be triaged')
+    parser.add_argument('--evidence_vcf_files', required=True)  # TODO: Add help here!
 
-    # parser.add_argument('--triage_vcf_file', required=True, help='VCF file to be triaged')
-    # parser.add_argument('--evidence_vcf_files', required=True, help='Comma-separated list of VCF files to be used as second opinions')
+    args = parser.parse_args()
 
-    # args = parser.parse_args()
+    evidence: Dict[str, List[Variant]] = {}
+    for vcf_id, vcf_file in [id_file_pair.split(':') for id_file_pair in args.evidence_vcf_files.split(',')]:
+        with gzip.open(vcf_file, 'rt') as f:
+            evidence[vcf_id] = parse_vcf(f).variants
 
-    # evidence: Dict[str, List[Variant]] = {}
-    # for evidence_vcf_file in args.evidence_vcf_files:
-    #     with gzip.open(evidence_vcf_file, 'rt') as f:
-    #         vcf = parse_vcf(evidence_vcf_file)
+    triager = Triager(evidence=evidence)
 
-    # triager = Triager()
+    with gzip.open(args.triage_vcf_file, 'rt') as f:
+        triager.triage(vcf=parse_vcf(f))
 
-    # with gzip.open(args.triage_vcf, 'rt') as f:
     #     for line in f:
     #         if line.startswith('#'):
     #             print(line.rstrip('\n'))
--- src/vartriage/triager.py
+++ src/vartriage/triager.py
@@ -1,9 +1,21 @@
 from typing import Dict, List
 
-from vartriage.core import Variant
+from vartriage.core import Variant, VCF
 
 
 class Triager:
 
     def __init__(self, evidence: Dict[str, List[Variant]]) -> None:
         self._evidence = evidence
+
+    def triage(self, vcf: VCF) -> VCF:
+
+        vcf.add_info_field(id_='VTSO',
+                           number='.',
+                           type_='Integer',
+                           description='Variant considered PASS based on second opinion from these callers (vartriage)')
+
+        vcf.add_info_filed(id_='VTOF',
+                           number='.',
+                           type_='String',
+                           description='Original FILTER before second opinion (vartriage)')
