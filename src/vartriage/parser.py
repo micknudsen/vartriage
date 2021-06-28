060c9d9c48294dcf58da1705bf2bf3c52f8ea52a
Michael Knudsen <micknudsen@gmail.com>
2021-06-28 07:59:12 +0200

    Implemented parse_vcf function

--- src/vartriage/parser.py
+++ src/vartriage/parser.py
@@ -4,4 +4,19 @@ from vartriage.core import VCF
 
 
 def parse_vcf(stream: Iterable[str]) -> VCF:
-    pass
+
+    header = []
+    columns = []
+    data = []
+
+    for row in stream:
+        if row.startswith('##'):
+            header.append(row)
+        elif row.startswith('#'):
+            columns.append(row)
+        else:
+            data.append(row)
+
+    return VCF(header=header,
+               columns=columns,
+               data=data)
