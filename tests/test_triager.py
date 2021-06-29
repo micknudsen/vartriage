84117e4142a25d521143c1493cb062716f3715a8
Michael Knudsen <micknudsen@gmail.com>
2021-06-29 08:46:57 +0200

    Added first triage test

--- tests/test_triager.py
+++ tests/test_triager.py
@@ -1,23 +1,42 @@
 import unittest
 
-from vartriage.core import Variant
-
+from vartriage.core import Variant, VCF
 from vartriage.triager import Triager
 
 
 class TestTriager(unittest.TestCase):
 
-    def setUp(self):
+    def test_triage_filtered_variant_which_passes_in_evidence(self):
 
-        evidence_variants = [Variant(chrom='chr1', pos='9611912', id_='.', ref='A', alt='T', qual='.', filter_='LowEVS', info='SOMATIC;QSS=2', format_='DP:FDP:SDP', samples={'NORMAL': '68:9:0', 'TUMOR': '273:28:0'}),
-                             Variant(chrom='chr9', pos='129439117', id_='.', ref='T', alt='C', qual='.', filter='PASS', info='SOMATIC;QSS=34', format_='DP:FDP:SDP', samples={'NORMAL': '21:0:0:0:0,0:1,1:0,0:20,20', 'TUMOR': '80:12:0:0:0,0:19,30:0,0:49,50'}),
-                             Variant(chrom='chr15', pos='93301718', id_='.', ref='G', alt='T', qual='.', filter='PASS', info='SOMATIC;QSS=35', format_='DP:FDP:SDP', samples={'NORMAL': '9:0:0:0:0,0:0,0:9,9:0,0', 'TUMOR': '27:0:0:0:0,0:0,0:22,23:5,5'}),
-                             Variant(chrom='chr20', pos='30388785', id_='.', ref='T', alt='G', qual='.', filter='LowEVS', info='SOMATIC;QSS=5', format='DP:FDP:SDP', samples={'NORMAL': '9:0:0:0:0,0:0,0:0,0:9,18', 'TUMOR': '53:2:0:0:0,0:0,0:3,4:48,69'})]
+        vcf = VCF(
+            header=[],
+            sample_names=[],
+            variants=[
+                Variant(chrom='chr1', pos=100, id_='.', ref='A', alt='T', qual='.', filter_='clustered_events', info='', format_='', samples={})
+            ]
+        )
 
-        self.triager = Triager()
+        triager = Triager(evidence={
+            'X': [
+                Variant(chrom='chr1', pos=100, id_='.', ref='A', alt='T', qual='.', filter_='PASS', info='', format_='', samples={})
+            ]
+        })
 
-        for variant in evidence_variants:
-            self.triager.add_evidence(variant=variant)
+        triager.triage(vcf=vcf)
+
+        self.assertFalse(vcf.variants[0].is_filtered())
+        self.assertEqual(vcf.variants[0].get_info('VTSO'), 'X')
+        self.assertEqual(vcf.variants[0].get_ifno('VTOF'), 'clustered_events')
+
+        # evidence_variants = [Variant(chrom='chr1', pos='9611912', id_='.', ref='A', alt='T', qual='.', filter_='LowEVS', info='SOMATIC;QSS=2', format_='DP:FDP:SDP', samples={'NORMAL': '68:9:0', 'TUMOR': '273:28:0'}),
+        #                      Variant(chrom='chr9', pos='129439117', id_='.', ref='T', alt='C', qual='.', filter='PASS', info='SOMATIC;QSS=34', format_='DP:FDP:SDP', samples={'NORMAL': '21:0:0:0:0,0:1,1:0,0:20,20', 'TUMOR': '80:12:0:0:0,0:19,30:0,0:49,50'}),
+        #                      Variant(chrom='chr15', pos='93301718', id_='.', ref='G', alt='T', qual='.', filter='PASS', info='SOMATIC;QSS=35', format_='DP:FDP:SDP', samples={'NORMAL': '9:0:0:0:0,0:0,0:9,9:0,0', 'TUMOR': '27:0:0:0:0,0:0,0:22,23:5,5'}),
+        #                      Variant(chrom='chr20', pos='30388785', id_='.', ref='T', alt='G', qual='.', filter='LowEVS', info='SOMATIC;QSS=5', format='DP:FDP:SDP', samples={'NORMAL': '9:0:0:0:0,0:0,0:0,0:9,18', 'TUMOR': '53:2:0:0:0,0:0,0:3,4:48,69'})]
+
+        # self.triager = Triager()
+
+        # for variant in evidence_variants:
+        #     self.triager.add_evidence(variant=variant)
 
 #     def test_triage_pass_variant_which_passes_in_evidence(self):
 #         variant = parse_vcf_entry(entry='\t'.join(['chr15', '93301718', '.', 'G', 'T', '.', 'PASS', 'AS_FilterStatus=SITE;AS_SB_TABLE=0,31|0,5;DP=37;ECNT=1;GERMQ=42;MBQ=36,36;MFRL=289,327;MMQ=60,60;MPOS=22;NALOD=1.000;NLOD=2.71;POPAF=6.00;ROQ=81;TLOD=13.99', 'GT:AD:AF:DP:F1R2:F2R1:SB', '0/0:9,0:0.091:9:4,0:5,0:0,9,0,0', '0/1:22,5:0.207:27:8,3:13,2:0,22,0,5']))
