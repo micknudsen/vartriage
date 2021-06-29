3892e02cb1673d67220d93fafdf79319f67b7e16
Michael Knudsen <micknudsen@gmail.com>
2021-06-29 10:51:10 +0200

    Test all the things!

--- tests/test_triager.py
+++ tests/test_triager.py
@@ -51,10 +51,92 @@ class TestTriager(unittest.TestCase):
         self.assertIs(vcf.variants[0].get_info('VTOF'), False)
 
     def test_triager_pass_variants_which_also_passes_in_evidence(self):
-        self.assertTrue(False)
+
+        vcf = VCF(
+            header=[],
+            sample_names=[],
+            variants=[
+                Variant(chrom='chr1', pos='100', id_='.', ref='A', alt='T', qual='.', filter_='PASS', info='', format_='', samples={})
+            ]
+        )
+
+        triager = Triager(evidence={
+            'X': [
+                Variant(chrom='chr1', pos='100', id_='.', ref='A', alt='T', qual='.', filter_='haplotype', info='', format_='', samples={})
+            ]
+        })
+
+        triager.triage(vcf=vcf)
+
+        self.assertFalse(vcf.variants[0].is_filtered())
+        self.assertIs(vcf.variants[0].get_info('VTSO'), False)
+        self.assertIs(vcf.variants[0].get_info('VTOF'), False)
 
     def test_triager_pass_variants_which_fails_in_evidence(self):
-        self.assertTrue(False)
+
+        vcf = VCF(
+            header=[],
+            sample_names=[],
+            variants=[
+                Variant(chrom='chr1', pos='100', id_='.', ref='A', alt='T', qual='.', filter_='PASS', info='', format_='', samples={})
+            ]
+        )
+
+        triager = Triager(evidence={
+            'X': [
+                Variant(chrom='chr1', pos='100', id_='.', ref='A', alt='T', qual='.', filter_='PASS', info='', format_='', samples={})
+            ]
+        })
+
+        triager.triage(vcf=vcf)
+
+        self.assertFalse(vcf.variants[0].is_filtered())
+        self.assertIs(vcf.variants[0].get_info('VTSO'), False)
+        self.assertIs(vcf.variants[0].get_info('VTOF'), False)
 
     def test_triager_multiple_variants_multiple_evidence(self):
-        self.assertTrue(False)
+
+        vcf = VCF(
+            header=[],
+            sample_names=[],
+            variants=[
+                Variant(chrom='chr1', pos='100', id_='.', ref='A', alt='T', qual='.', filter_='PASS', info='', format_='', samples={}),
+                Variant(chrom='chr2', pos='500', id_='.', ref='G', alt='GATATA', qual='.', filter_='weak_evidence', info='', format_='', samples={}),
+                Variant(chrom='chr3', pos='7500', id_='.', ref='ACT', alt='A', qual='.', filter_='germline', info='', format_='', samples={}),
+                Variant(chrom='chr4', pos='1980', id_='.', ref='GGGG', alt='G', qual='.', filter_='haplotype', info='', format_='', samples={}),
+                Variant(chrom='chr5', pos='5700', id_='.', ref='CTCTCT', alt='C', qual='.', filter_='slippage', info='', format_='', samples={}),
+            ]
+        )
+
+        triager = Triager(evidence={
+            'X': [
+                Variant(chrom='chr2', pos='500', id_='.', ref='G', alt='GATATA', qual='.', filter_='PASS', info='', format_='', samples={}),
+                Variant(chrom='chr3', pos='7500', id_='.', ref='ACT', alt='A', qual='.', filter_='contamination', info='', format_='', samples={})
+            ],
+            'Y': [
+                Variant(chrom='chr2', pos='500', id_='.', ref='G', alt='GATATA', qual='.', filter_='PASS', info='', format_='', samples={}),
+                Variant(chrom='chr4', pos='1980', id_='.', ref='GGGG', alt='G', qual='.', filter_='PASS', info='', format_='', samples={}),
+            ]
+        })
+
+        triager.triage(vcf=vcf)
+
+        self.assertFalse(vcf.variants[0].is_filtered())
+        self.assertIs(vcf.variants[0].get_info('VTSO'), False)
+        self.assertIs(vcf.variants[0].get_info('VTOF'), False)
+
+        self.assertFalse(vcf.variants[1].is_filtered())
+        self.assertEqual(vcf.variants[1].get_info('VTSO'), 'X,Y')
+        self.assertEqual(vcf.variants[1].get_info('VTOF'), 'weak_evidence')
+
+        self.assertTrue(vcf.variants[2].is_filtered())
+        self.assertIs(vcf.variants[2].get_info('VTSO'), False)
+        self.assertIs(vcf.variants[2].get_info('VTOF'), False)
+
+        self.assertFalse(vcf.variants[3].is_filtered())
+        self.assertEqual(vcf.variants[3].get_info('VTSO'), 'Y')
+        self.assertEqual(vcf.variants[3].get_info('VTOF'), 'haplotype')
+
+        self.assertTrue(vcf.variants[4].is_filtered())
+        self.assertIs(vcf.variants[4].get_info('VTSO'), False)
+        self.assertIs(vcf.variants[4].get_info('VTOF'), False)
