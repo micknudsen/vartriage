18e5d6180b4246e3f1108145fe18e1c4fb4a76d3
Michael Knudsen <micknudsen@gmail.com>
2021-06-29 10:21:25 +0200

    Implemented triage method

--- src/vartriage/triager.py
+++ src/vartriage/triager.py
@@ -8,14 +8,15 @@ class Triager:
     def __init__(self, evidence: Dict[str, List[Variant]]) -> None:
         self._evidence = evidence
 
-    def triage(self, vcf: VCF) -> VCF:
+    def triage(self, vcf: VCF) -> None:
 
-        vcf.add_info_field(id_='VTSO',
-                           number='.',
-                           type_='Integer',
-                           description='Variant considered PASS based on second opinion from these callers (vartriage)')
+        vcf.add_info_field(id_='VTSO', number='.', type_='Integer', description='Variant considered PASS based on second opinion from these callers (vartriage)')
+        vcf.add_info_field(id_='VTOF', number='.', type_='String', description='Original FILTER before second opinion (vartriage)')
 
-        vcf.add_info_field(id_='VTOF',
-                           number='.',
-                           type_='String',
-                           description='Original FILTER before second opinion (vartriage)')
+        for variant in vcf.variants:
+            if variant.is_filtered():
+                second_opinions = [evidence_id for evidence_id, evidence_variants in self._evidence.items() if variant in evidence_variants]
+                if second_opinions:
+                    variant.set_info('VTSO', ','.join(second_opinions))
+                    variant.set_info('VTOF', variant.filter_)
+                    variant.filter_ = 'PASS'
