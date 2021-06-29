a05dfa5f4392fb85e56e4834bafe682a7d91746e
Michael Knudsen <micknudsen@gmail.com>
2021-06-29 09:44:53 +0200

    Test case where info field is not set

--- tests/test_core.py
+++ tests/test_core.py
@@ -40,6 +40,9 @@ class TestVariant(unittest.TestCase):
     def test_variant_get_info(self):
         self.assertEqual(self.variant.get_info('DP'), '236')
 
+    def test_variant_get_info_not_set(self):
+        self.assertIs(self.variant.get_info('TLOD'), False)
+
     def test_variant_set_info_with_value(self):
         self.variant.set_info(key='TLOD', value='3.14')
         self.assertEqual(self.variant.get_info('TLOD'), '3.14')
