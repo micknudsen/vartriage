b2a30a4c06ddc8a7e2bbd315a3e5e92b1f550a14
Michael Knudsen <micknudsen@gmail.com>
2021-06-29 10:37:38 +0200

    Bugfix

--- src/vartriage/triager.py
+++ src/vartriage/triager.py
@@ -14,9 +14,21 @@ class Triager:
         vcf.add_info_field(id_='VTOF', number='.', type_='String', description='Original FILTER before second opinion (vartriage)')
 
         for variant in vcf.variants:
+
             if variant.is_filtered():
-                second_opinions = [evidence_id for evidence_id, evidence_variants in self._evidence.items() if variant in evidence_variants]
+
+                second_opinions = []
+
+                for evidence_id, evidence_variants in self._evidence.items():
+
+                    for variant_ in evidence_variants:
+                        if not variant_.is_filtered() and variant_ == variant:
+                            second_opinions.append(evidence_id)
+                            break
+
                 if second_opinions:
+
                     variant.set_info('VTSO', ','.join(second_opinions))
                     variant.set_info('VTOF', variant.filter_)
+
                     variant.filter_ = 'PASS'
