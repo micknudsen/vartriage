e675ee0ffd8ceb0c28566618539a03cf73f8f247
Michael Knudsen <micknudsen@gmail.com>
2021-06-29 10:33:54 +0200

    Test that filtered variants do not count as second opinion

--- tests/test_triager.py
+++ tests/test_triager.py
@@ -27,3 +27,25 @@ class TestTriager(unittest.TestCase):
         self.assertFalse(vcf.variants[0].is_filtered())
         self.assertEqual(vcf.variants[0].get_info('VTSO'), 'X')
         self.assertEqual(vcf.variants[0].get_info('VTOF'), 'clustered_events')
+
+    def test_triager_filtered_variant_which_is_also_filtered_in_evidence(self):
+
+        vcf = VCF(
+            header=[],
+            sample_names=[],
+            variants=[
+                Variant(chrom='chr1', pos='100', id_='.', ref='A', alt='T', qual='.', filter_='clustered_events', info='', format_='', samples={})
+            ]
+        )
+
+        triager = Triager(evidence={
+            'X': [
+                Variant(chrom='chr1', pos='100', id_='.', ref='A', alt='T', qual='.', filter_='LowEVS', info='', format_='', samples={})
+            ]
+        })
+
+        triager.triage(vcf=vcf)
+
+        self.assertTrue(vcf.variants[0].is_filtered())
+        self.assertIs(vcf.variants[0].get_info('VTSO'), False)
+        self.assertIs(vcf.variants[0].get_info('VTOF'), False)
